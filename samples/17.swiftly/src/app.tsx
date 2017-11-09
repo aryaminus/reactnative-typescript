49c43567f4450761bfaffee324e03a9bd1596c1c
Rodrigo Landaeta <rlandaeta@cochlear.com>
2017-11-09 16:06:46 +1100

    Replicated the project structure in swift.

---
+++ samples/17.swiftly/src/app.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { AppRegistry, View } from "react-native";
+
+const App = () => {
+    return (
+        <View>
+        </View>
+    );
+};
+
+AppRegistry.registerComponent("swiftly", () => App);
