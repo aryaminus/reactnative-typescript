64ad9d2da00e55056d243da450a941271bbf02e7
Rodrigo Landaeta <rlandaeta@cochlear.com>
2017-11-10 11:38:13 +1100

    Creating the basic react-native template without the iOS project definition (as it will be created with swift)

---
+++ samples/17.swiftly/src/app.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { AppRegistry, View } from "react-native";
+
+const App = () => {
+    return (
+        <View></View>
+    );
+}
+
+AppRegistry.registerComponent("swiftly", () => App);
